5d3f668ccab5b79a35cd19b8ad73e719e6a10e28
DURGESH PAL <michaeldurgesh@gmail.com>
2023-10-04 00:49:52 +0530

    Update palindrome.cpp

--- C++/palindrome.cpp
+++ C++/palindrome.cpp
@@ -26,3 +26,25 @@ int main()
 
     return 0;
 }
+
+
+
+
+Another code for Palindrome in java
+
+class Palindrome{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;    
+  int n=656;    // number variable to be checked for palindrome  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
